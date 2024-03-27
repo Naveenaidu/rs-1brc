9a80fda1d092650b3f82ad854e34f21f4b7a281b
Naveen Naidu <naveennaidu479@gmail.com>
2024-03-27 10:20:17 +0530

    naive implementation

---
+++ src/Notes.md
@@ -0,0 +1,14 @@
+There are different ways to read the file, we might have to compare their performance.
+
+Testing:
+-> Comparing the diff will not work
+-> Read through the .out file spit by program and the test file and then compare 
+   the hashmap.
+
+-> A program, which takes a file and gives us back a hashmap
+
+TODO:
+1. Write the values to file -> With one round off digit
+2. Write the code for test case to compare the values with the *.out file
+    -> We can just read the *.out file into a hashmap
+    -> We can then compare the hashmap
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+use std::{collections::HashMap, hash::Hash};
+
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "rs-1brc",
+    version = "1.0",
+    about = "confusedHooman's version of 1BRC challenge"
+)]
+struct Args {
+    #[arg(short = 'f', long, help = "Path to the measurement file")]
+    file: String,
+}
+
+#[derive(Debug)]
+struct StationValues{
+    min: f32,
+    max: f32,
+    mean: f32,
+    count: u32,
+}
+
+fn read_line(data: String) -> (String, f32){
+    let parts: Vec<&str> = data.split(';').collect();
+    let station_name = parts[0].to_string();
+    let value = parts[1].parse::<f32>().expect("Failed to parse value");
+    (station_name, value)
+}
+
+// Calculate the station values
+// For new entry: The min and max and mean would be the same
+// For existing entry: The min and max would be updated if the new value is less than min or greater than max
+// TODO: mean has to be calculated at the END
+fn calculate_station_values(data: String) -> HashMap<String, StationValues> {
+    let mut result: HashMap<String, StationValues> = HashMap::new();
+    for line in data.lines() {
+        let (station_name, value) = read_line(line.to_string());
+        result.entry(station_name)
+            .and_modify(|e| {
+                if value < e.min {
+                    e.min = value;
+                }
+                if value > e.max {
+                    e.max = value;
+                }
+                e.mean = e.mean + value;
+                e.count += 1;
+            })
+            .or_insert(StationValues {
+                min: value,
+                max: value,
+                mean: value,
+                count: 1,
+            });
+    }
+
+    // Calculate the mean for all entries
+    for (_, station_values) in result.iter_mut() {
+        station_values.mean = station_values.mean / station_values.count as f32;
+    }
+
+    result
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let data = std::fs::read_to_string(args.file).expect("Failed to read file");
+    let result = calculate_station_values(data);
+    print!("{:?}", result);
 }
