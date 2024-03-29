9732e4742d92e9b5eba271cb27afa054ef7a5e1f
Naveen Naidu <naveennaidu479@gmail.com>
2024-03-29 17:09:44 +0530

    switch to decimal to allow proper rounding of numbers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, os::linux::raw::stat};
+use rust_decimal::{Decimal, RoundingStrategy};
+use rust_decimal_macros::dec;
+
 
 use clap::Parser;
 
@@ -16,16 +19,19 @@ struct Args {
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 struct StationValues{
-    min: f32,
-    max: f32,
-    mean: f32,
-    count: u32,
+    // We want all values rounded to 1 decimal place  using the semantics of IEEE 754 rounding-direction "roundTowardPositive"
+    // Note: We use Decimal instaed of Floats, because it's easier to round up the fractional part of Decimals instead of floats
+    // Read later: https://users.rust-lang.org/t/why-doesnt-round-have-an-argument-for-digits/100688/24
+    min: Decimal,
+    max: Decimal,
+    mean: Decimal,
+    count: Decimal,
 }
 
-fn read_line(data: String) -> (String, f32){
+fn read_line(data: String) -> (String, Decimal){
     let parts: Vec<&str> = data.split(';').collect();
     let station_name = parts[0].to_string();
-    let value = parts[1].parse::<f32>().expect("Failed to parse value");
+    let value = parts[1].parse::<Decimal>().expect("Failed to parse value");
     (station_name, value)
 }
 
@@ -46,19 +52,22 @@ fn calculate_station_values(data: String) -> HashMap<String, StationValues> {
                     e.max = value;
                 }
                 e.mean = e.mean + value;
-                e.count += 1;
+                e.count += dec!(1);
             })
             .or_insert(StationValues {
                 min: value,
                 max: value,
                 mean: value,
-                count: 1,
+                count: dec!(1),
             });
     }
 
     // Calculate the mean for all entries
     for (_, station_values) in result.iter_mut() {
-        station_values.mean = station_values.mean / station_values.count as f32;
+        station_values.max = station_values.max.round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
+        station_values.min = station_values.min.round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
+        station_values.mean = (station_values.mean / station_values.count).round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
+
     }
 
     result
@@ -73,11 +82,16 @@ fn main() {
     let result = calculate_station_values(data);
     print!("{:?}", result);
 
-    let test_output = read_test_output_file("tests/measurements-10.out".to_string());
+    print!("\n-----------------------------------\n");
+
+    let test_output = read_test_output_file_tmp("tests/measurements-3.out".to_string());
     print!("{:?}", test_output);
+
+
 }
 
-fn read_test_output_file(file_name: String) -> HashMap<String, StationValues> {
+// TODO: Naveen: remove this function
+fn read_test_output_file_tmp(file_name: String) -> HashMap<String, StationValues> {
     let data = std::fs::read_to_string(file_name).expect("Failed to read file");
     // remove whitespace and braces
     // {Adelaide=15.0/15.0/15.0, Cabo San Lucas=14.9/14.9/14.9} => Adelaide=15.0/15.0/15.0, Cabo San Lucas=14.9/14.9/14.9
@@ -90,24 +104,52 @@ fn read_test_output_file(file_name: String) -> HashMap<String, StationValues> {
     // split the data by "=" and "/" to get the station name and values
     for station_data in stations_data {
         let parts: Vec<&str> = station_data.split("=").collect();
-        let station_name = parts[0].to_string();
+        // Remove whitespace from the station name
+        let station_name = parts[0].trim_start().trim_end().to_string();
         let values: Vec<&str> = parts[1].split("/").collect();
-        let min = values[0].parse::<f32>().expect("Failed to parse min");
-        let max = values[1].parse::<f32>().expect("Failed to parse max");
-        let mean = values[2].parse::<f32>().expect("Failed to parse mean");
-        result.insert(station_name, StationValues{min, max, mean, count:0});
+        let min = values[0].parse::<Decimal>().expect("Failed to parse min");
+        let mean = values[1].parse::<Decimal>().expect("Failed to parse mean");
+        let max = values[2].parse::<Decimal>().expect("Failed to parse max");
+        result.insert(station_name, StationValues{min, max, mean, count:dec!(0)});
     }
-    result
-
-    
+    result    
 }
+
 
 #[cfg(test)]
 mod tests {
     use std::{fs, path::PathBuf};
+    use std::collections::HashMap;
 
-    use crate::{calculate_station_values, read_test_output_file};
 
+    use rust_decimal::Decimal;
+    use rust_decimal_macros::dec;
+
+    use crate::{calculate_station_values, StationValues};
+
+    fn read_test_output_file(file_name: String) -> HashMap<String, StationValues> {
+        let data = std::fs::read_to_string(file_name).expect("Failed to read file");
+        // remove whitespace and braces
+        // {Adelaide=15.0/15.0/15.0, Cabo San Lucas=14.9/14.9/14.9} => Adelaide=15.0/15.0/15.0, Cabo San Lucas=14.9/14.9/14.9
+        let data_without_braces = data.trim_start().trim_end().trim_matches(['{', '}'].as_ref());
+    
+        // split the data by comma
+        // Adelaide=15.0/15.0/15.0, Cabo San Lucas=14.9/14.9/14.9 => [Adelaide=15.0/15.0/15.0, Cabo San Lucas=14.9/14.9/14.9]
+        let stations_data: Vec<&str> = data_without_braces.split(",").collect();  
+        let mut result: HashMap<String, StationValues> = HashMap::new();
+        // split the data by "=" and "/" to get the station name and values
+        for station_data in stations_data {
+            let parts: Vec<&str> = station_data.split("=").collect();
+            let station_name = parts[0].trim_start().trim_end().to_string();
+            let values: Vec<&str> = parts[1].split("/").collect();
+            let min = values[0].parse::<Decimal>().expect("Failed to parse min");
+            let mean = values[1].parse::<Decimal>().expect("Failed to parse mean");
+            let max = values[2].parse::<Decimal>().expect("Failed to parse max");
+            result.insert(station_name, StationValues{min, max, mean, count:dec!(0)});
+        }
+        result    
+    }
+    
 
     #[test]
     fn test_measurement_data() {
