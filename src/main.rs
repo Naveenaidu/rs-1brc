c1eed0fcda256984d5b1240b9897bac2804f7bb5
Naveen Naidu <naveennaidu479@gmail.com>
2024-04-10 09:49:50 +0530

    Appraoch 2: Use BufferedRead and remove print
    The print!() was in the hot path i.e it was in the for loop.
    The print!() is not advised to be had in the hot path as it needs
    to talk to OS to keep asking for locks. This slows down the process.
    
    The better solution might have been to lock the stdin and then use it.
    I did not try that. TODO: Keep that for future.
    
    Time: ~20 minutes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use opentelemetry::{
     global::{self},
-    trace::{TraceContextExt, Tracer},
+    // trace::{TraceContextExt, Tracer},
     KeyValue,
 };
 use opentelemetry_otlp::WithExportConfig;
@@ -37,234 +37,101 @@ struct StationValues {
 
 fn read_line(data: String) -> (String, Decimal) {
     let parts: Vec<&str> = data.split(';').collect();
-    print!("{:?} | parts: {:?} \n", data, parts);
     let station_name = parts[0].to_string();
     let value = parts[1].parse::<Decimal>().expect("Failed to parse value");
     (station_name, value)
 }
 
-pub fn init_tracer() {
-    let otlp_exporter = opentelemetry_otlp::new_exporter()
-        .tonic()
-        .with_endpoint("http://localhost:4317");
-    let tracer = opentelemetry_otlp::new_pipeline()
-        .tracing()
-        .with_exporter(otlp_exporter)
-        .with_trace_config(opentelemetry::sdk::trace::config().with_resource(
-            opentelemetry_sdk::Resource::new(vec![
-                KeyValue::new(semcov::resource::SERVICE_NAME, "rs-1brc"),
-                KeyValue::new(
-                    semcov::resource::SERVICE_VERSION,
-                    env!("CARGO_PKG_VERSION").to_string(),
-                ),
-            ]),
-        ))
-        .install_simple()
-        .expect("Failed to install tracer");
-    global::set_tracer_provider(tracer.provider().unwrap());
-}
-
-// Calculate the station values
-// For new entry: The min and max and mean would be the same
-// For existing entry: The min and max would be updated if the new value is less than min or greater than max. And mean will be calculated after getting all count
-fn calculate_station_values(data: String) -> HashMap<String, StationValues> {
-    let tracer = global::tracer("rs-1brc");
-    let mut result: HashMap<String, StationValues> = HashMap::new();
-    tracer.in_span("calculate_station_values", |_ctx| {
-        for line in data.lines() {
-            let (station_name, value) = read_line(line.to_string());
-            result
-                .entry(station_name)
-                .and_modify(|e| {
-                    if value < e.min {
-                        e.min = value;
-                    }
-                    if value > e.max {
-                        e.max = value;
-                    }
-                    e.mean = e.mean + value;
-                    e.count += dec!(1);
-                })
-                .or_insert(StationValues {
-                    min: value,
-                    max: value,
-                    mean: value,
-                    count: dec!(1),
-                });
-        }
-
-        tracer.in_span("calculate mean", |_ctx| {
-            // Calculate the mean for all entries
-            for (_, station_values) in result.iter_mut() {
-                station_values.max = station_values
-                    .max
-                    .round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
-                station_values.min = station_values
-                    .min
-                    .round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
-                station_values.mean = (station_values.mean / station_values.count)
-                    .round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
-            }
-        })
-    });
-
-    result
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn error::Error>> {
+fn main() -> Result<(), Box<dyn error::Error>> {
     let args = Args::parse();
-    init_tracer();
-    let tracer = global::tracer("rs-1brc");
-    tracer.in_span("main", |cx| {
-        cx.span().set_attribute(opentelemetry::KeyValue {
-            key: "hello".into(),
-            value: "world".into(),
-        });
 
-        // let mut file_content = Vec::new();
-        let file = std::fs::File::open(&args.file).expect("Failed to open file");
-        let reader = BufReader::new(file);
-        let mut result: HashMap<String, StationValues> = HashMap::new();
-        for line in reader.lines(){
-            let line = line.expect("Failed to read line");
-            let (station_name, value) = read_line(line);
-            // print!("{:?} | {:?}\n", station_name, value);
+    let file = std::fs::File::open(&args.file).expect("Failed to open file");
+    let reader = BufReader::new(file);
+    let mut result: HashMap<String, StationValues> = HashMap::new();
+    for line in reader.lines(){
+        let line = line.expect("Failed to read line");
+        let (station_name, value) = read_line(line);
 
-            result
-                .entry(station_name)
-                .and_modify(|e| {
-                    if value < e.min {
-                        e.min = value;
-                    }
-                    if value > e.max {
-                        e.max = value;
-                    }
-                    e.mean = e.mean + value;
-                    e.count += dec!(1);
-                })
-                .or_insert(StationValues {
-                    min: value,
-                    max: value,
-                    mean: value,
-                    count: dec!(1),
-                });
+        result
+            .entry(station_name)
+            .and_modify(|e| {
+                if value < e.min {
+                    e.min = value;
+                }
+                if value > e.max {
+                    e.max = value;
+                }
+                e.mean = e.mean + value;
+                e.count += dec!(1);
+            })
+            .or_insert(StationValues {
+                min: value,
+                max: value,
+                mean: value,
+                count: dec!(1),
+            });
 
-        }
-        println!("{:?}", result);
-        // std::io::Read::read_to_end(&mut file, &mut file_content)
-        //     .expect("Failed to read file content");
+    }
 
+    for (_, station_values) in result.iter_mut() {
+        station_values.max = station_values
+            .max
+            .round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
+        station_values.min = station_values
+            .min
+            .round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
+        station_values.mean = (station_values.mean / station_values.count)
+            .round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
+    }
 
-        // let mut result: HashMap<String, StationValues> = HashMap::new();
-        // for chunk in file_content.split(|&x| x == b'\n') {
-        //     // For last line
-        //     if chunk.is_empty() {
-        //         continue;
-        //     }
-        //     let mut line_bytes = chunk.split(|&x| x == b';');
-        //     let station_name_bytes = line_bytes.next().expect("Failed to get station name");
-        //     let station_name = String::from_utf8(station_name_bytes.to_vec())
-        //         .expect("Failed to convert to string");
-
-        //     let value_bytes = line_bytes.next().expect("Failed to get value");
-        //     let value = String::from_utf8(value_bytes.to_vec())
-        //         .expect("Failed to convert to string")
-        //         .parse::<Decimal>()
-        //         .expect("Failed to parse value");
+    print!("{:?}", result);
+    
 
 
-        //     let line = String::from_utf8(chunk.to_vec()).expect("Failed to convert to string");
-        //     print!("{:?}\n", line);
-        //     // let (station_name, value) = read_line(line);
-        //     result
-        //         .entry(station_name)
-        //         .and_modify(|e| {
-        //             if value < e.min {
-        //                 e.min = value;
-        //             }
-        //             if value > e.max {
-        //                 e.max = value;
-        //             }
-        //             e.mean = e.mean + value;
-        //             e.count += dec!(1);
-        //         })
-        //         .or_insert(StationValues {
-        //             min: value,
-        //             max: value,
-        //             mean: value,
-        //             count: dec!(1),
-        //         });
-            
-            
-        // }
-
-        // for (_, station_values) in result.iter_mut() {
-        //     station_values.max = station_values
-        //         .max
-        //         .round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
-        //     station_values.min = station_values
-        //         .min
-        //         .round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
-        //     station_values.mean = (station_values.mean / station_values.count)
-        //         .round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
-        // }
-
-
-
-
-
-    });
-
-    global::shutdown_tracer_provider();
-
-    // print!("\n-----------------------------------\n");
-    // let test_output = crate::util::read_test_output_file("tests/measurements-20.out".to_string());
-    // print!("{:?}", test_output);
     Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use std::{fs, path::PathBuf};
+// #[cfg(test)]
+// mod tests {
+//     use std::{fs, path::PathBuf};
 
-    use crate::calculate_station_values;
-    use crate::util::read_test_output_file;
+//     use crate::calculate_station_values;
+//     use crate::util::read_test_output_file;
 
-    #[test]
-    fn test_measurement_data() {
-        let test_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests");
-        let files = fs::read_dir(test_dir).unwrap();
+//     #[test]
+//     fn test_measurement_data() {
+//         let test_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests");
+//         let files = fs::read_dir(test_dir).unwrap();
 
-        for file in files {
-            let test_file_name = file.unwrap().path().to_str().unwrap().to_string();
-            if test_file_name.ends_with(".out") {
-                continue;
-            }
-            let output_file_name = test_file_name.replace(".txt", ".out");
-            print!("\nTest file: {}\n", test_file_name);
-            let test_output = read_test_output_file(output_file_name);
-            let data = fs::read_to_string(test_file_name.clone()).expect("Failed to read file");
-            let mut result = calculate_station_values(data);
-            let mut test_output_map_copy = test_output.clone();
+//         for file in files {
+//             let test_file_name = file.unwrap().path().to_str().unwrap().to_string();
+//             if test_file_name.ends_with(".out") {
+//                 continue;
+//             }
+//             let output_file_name = test_file_name.replace(".txt", ".out");
+//             print!("\nTest file: {}\n", test_file_name);
+//             let test_output = read_test_output_file(output_file_name);
+//             let data = fs::read_to_string(test_file_name.clone()).expect("Failed to read file");
+//             let mut result = calculate_station_values(data);
+//             let mut test_output_map_copy = test_output.clone();
 
-            // compare two hashmaps
-            for (station_name, station_values) in test_output.into_iter() {
-                let result_station_values = result.remove(&station_name).expect(
-                    ("Station not found: ".to_string() + &station_name + " in result hashmap")
-                        .as_str(),
-                );
-                assert_eq!(station_values.min, result_station_values.min);
-                assert_eq!(station_values.max, result_station_values.max);
-                assert_eq!(station_values.mean, result_station_values.mean);
-                test_output_map_copy.remove(&station_name);
-            }
+//             // compare two hashmaps
+//             for (station_name, station_values) in test_output.into_iter() {
+//                 let result_station_values = result.remove(&station_name).expect(
+//                     ("Station not found: ".to_string() + &station_name + " in result hashmap")
+//                         .as_str(),
+//                 );
+//                 assert_eq!(station_values.min, result_station_values.min);
+//                 assert_eq!(station_values.max, result_station_values.max);
+//                 assert_eq!(station_values.mean, result_station_values.mean);
+//                 test_output_map_copy.remove(&station_name);
+//             }
 
-            assert_eq!(result.len(), 0);
-            assert_eq!(test_output_map_copy.len(), 0);
+//             assert_eq!(result.len(), 0);
+//             assert_eq!(test_output_map_copy.len(), 0);
 
-            print!("Test passed\n");
-            print!("-----------------------------------\n");
-        }
-    }
-}
+//             print!("Test passed\n");
+//             print!("-----------------------------------\n");
+//         }
+//     }
+// }
