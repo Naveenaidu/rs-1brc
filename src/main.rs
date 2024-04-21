c0e6d36afb73575030899461a7c4e38f5897e2b4
Naveen Naidu <naveennaidu479@gmail.com>
2024-04-21 19:57:39 +0530

    Load entire file in memory
    In this commit we do two things:
    1. Load entire file into memory and store them as byte
    2. And pass the slice of bytes to read_line instead of Vec, this saves
       us time from not clearing the vector
    
    Time: 48s

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 
 use rust_decimal::{prelude::FromPrimitive, Decimal, RoundingStrategy};
-use std::{ error, io::{BufRead, BufReader}, str::from_utf8};
+use std::{ error, io::{BufRead, BufReader, Read}, str::from_utf8};
 use rustc_hash::FxHashMap;
 
 mod util;
@@ -26,7 +26,7 @@ struct StationValues {
 }
 
 
-fn read_line(data: &Vec<u8>) -> (Vec<u8>, f32) {
+fn read_line(data: &[u8]) -> (Vec<u8>, f32) {
     let mut parts = data.rsplit(|&c| c == b';');
     let value_str = parts.next().expect("Failed to parse value string");
     let value = fast_float::parse(value_str).expect("Failed to parse value");
@@ -38,24 +38,19 @@ fn read_line(data: &Vec<u8>) -> (Vec<u8>, f32) {
 fn main() -> Result<(), Box<dyn error::Error>> {
     let args = Args::parse();
 
-    let file = std::fs::File::open(&args.file).expect("Failed to open file");
-    let mut reader = BufReader::new(file);
+    let mut file = std::fs::File::open(&args.file).expect("Failed to open file");
+    let mut buffer = Vec::new();
 
-    let mut result2: FxHashMap<Vec<u8>, StationValues> = FxHashMap::default();
-
-    let mut buf = Vec::new();
-    while let Ok(bytes_read) = reader.read_until(b'\n', &mut buf){
-        // Reached EOF
-        if bytes_read == 0 {
-            break;
+    file.read_to_end(&mut buffer).expect("Failed to read file");
+    let iter = buffer.split(|&c| c == b'\n');
+    let mut result: FxHashMap<Vec<u8>, StationValues> = FxHashMap::default();
+    for line in iter {
+        if line.len() == 0 {
+            continue;
         }
-        buf.truncate(bytes_read - 1);
-
-
-        // let line = String::from_utf8_lossy(&buf).trim();
-        let (station_name, value) = read_line(&buf);
+        let (station_name, value) = read_line(line);
         // println!("{:?} {:?}", station_name, value);
-        result2
+        result
             .entry(station_name)
             .and_modify(|e| {
                 if value < e.min {
@@ -73,17 +68,16 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                 mean: value,
                 count: 1.0,
             });
-        
-        buf.clear();
     }
 
-    for (_, station_values) in result2.iter_mut() {
+    for (_name, station_values) in result.iter_mut() {
         // We want all values rounded to 1 decimal place  using the semantics of IEEE 754 rounding-direction "roundTowardPositive"
         // Note: We use Decimal instaed of Floats, because it's easier to round up the fractional part of Decimals instead of floats
         // Read later: https://users.rust-lang.org/t/why-doesnt-round-have-an-argument-for-digits/100688/24
         let _max =  Decimal::from_f32(station_values.max).unwrap().round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
         let _min =  Decimal::from_f32(station_values.min).unwrap().round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
         let _mean =  Decimal::from_f32(station_values.mean/station_values.count).unwrap().round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
+        // println!("{:?};{:?};{:?};{:?}", from_utf8(name).unwrap(), _min, _mean, _max);
     }
 
 
