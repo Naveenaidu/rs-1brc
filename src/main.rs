90d6afbdaeb8b43528d8502b096d660d9d18dabb
Naveen Naidu <naveennaidu479@gmail.com>
2024-04-23 00:59:37 +0530

    use scoped threads, time 30s

--- src/main.rs
+++ src/main.rs
@@ -32,25 +32,8 @@ struct StationValues {
     count: f32,
 }
 
-
-fn read_line(data: &[u8]) -> (&[u8], f32) {
-    let mut parts = data.rsplit(|&c| c == b';');
-    let value_str = parts.next().expect("Failed to parse value string");
-    // let value = fast_float::parse(value_str).expect("Failed to parse value");
-    let value = match fast_float::parse(value_str) {
-        Ok(v) => v,
-        Err(e) => {
-            println!("{:?} {:?}", from_utf8(data).unwrap(), from_utf8(value_str));
-            0.0
-        }
-    };
-    let station_name = parts.next().expect("Failed to parse station name");
-    
-    (station_name, value)
-}
-
-fn process_chunk(data: Vec<u8>) -> FxHashMap<Vec<u8>, StationValues> {
-    let mut result: FxHashMap<Vec<u8>, StationValues> = FxHashMap::default();
+fn process_chunk(data: &[u8]) -> FxHashMap<&[u8], StationValues> {
+    let mut result: FxHashMap<&[u8], StationValues> = FxHashMap::default();
     let mut buffer = &data[..];
     // println!("processing chunk");
    loop {
@@ -65,7 +48,7 @@ fn process_chunk(data: Vec<u8>) -> FxHashMap<Vec<u8>, StationValues> {
                 let value = fast_float::parse(value).expect("Failed to parse value");
 
                 result
-                    .entry(name.to_vec())
+                    .entry(name)
                     .and_modify(|e| {
                         if value < e.min {
                             e.min = value;
@@ -96,49 +79,44 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
     let mut file = std::fs::File::open(&args.file).expect("Failed to open file");
     let mut buffer = Vec::new();
-    // println!("starting processing 2");
     file.read_to_end(&mut buffer).expect("Failed to read file");
-    // println!("starting processing 1");
 
-    let mut result: FxHashMap<Vec<u8>, StationValues> = FxHashMap::default();
+    let mut result: FxHashMap<&[u8], StationValues> = FxHashMap::default();
     let mut buffer = &buffer[..];
 
     let (tx, rx) = mpsc::channel();
-    // println!("starting processing");
 
 
     // count logical cores this process could try to use
     let mut chunks_to_create = num_cpus::get();
     let mut chunks_created = 0;
-    // println!("chunks to create: {:?}", chunks_to_create);
-    // let chunk_size = buffer.len() / num;
+
     while chunks_to_create > 0 && buffer.len() > 0 {
         let chunk_size = (buffer.len() / chunks_to_create) - 1;
         if buffer[chunk_size] == b'\n' {
-            let chunk = buffer[..chunk_size+1].to_vec();
+            let chunk = &buffer[..chunk_size+1];
             // spwan a thread to process chunk
-            // process_chunk(chunk);
-            // println!("spawning thread");
             let tx = tx.clone();
-            thread::spawn(move || {
-                let val = process_chunk(chunk);
-                tx.send(val).unwrap();
+            thread::scope(|scope| {
+                scope.spawn(move || {
+                    let val = process_chunk(chunk);
+                    tx.send(val).unwrap();
+                }); 
             });
 
             buffer = &buffer[chunk_size+1..];
             chunks_to_create -= 1;
             chunks_created += 1;
-
         } else {
             let newline = memchr(b'\n', &buffer[chunk_size..]).unwrap();
-            let chunk = buffer[..chunk_size+newline+1].to_vec();
+            let chunk = &buffer[..chunk_size+newline+1];
             // spwan a thread to process chunk
-            // process_chunk(chunk);
-            // println!("spawning thread");
             let tx = tx.clone();
-            thread::spawn(move || {
-                let val = process_chunk(chunk);
-                tx.send(val).unwrap();
+            thread::scope(|scope| {
+                scope.spawn(move || {
+                    let val = process_chunk(chunk);
+                    tx.send(val).unwrap();
+                }); 
             });
 
 
@@ -150,10 +128,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
     // println!("chunks created: {:?}", chunks_created);
     for i in 0..chunks_created{
-        // println!("---------------- {:?}", i);
         let val = rx.recv().unwrap();
-        
-        // println!("received chunk");
         for (station_name, station_values) in val.into_iter() {
             result
                 .entry(station_name)
@@ -179,7 +154,6 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         let _max =  Decimal::from_f32(station_values.max).unwrap().round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
         let _min =  Decimal::from_f32(station_values.min).unwrap().round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
         let _mean =  Decimal::from_f32(station_values.mean/station_values.count).unwrap().round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
-        // println!("{:?};{:?};{:?};{:?}", from_utf8(_name).unwrap(), _min, _mean, _max);
     }
 
 
