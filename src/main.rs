03238cd30268d7191b1b6b40feba5d8835d2bedd
Naveen Naidu <naveennaidu479@gmail.com>
2024-04-23 02:34:26 +0530

    use mmap, the main contention was reading the file. time: 15s

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use clap::Parser;
 
+use memmap::Mmap;
 use rust_decimal::{prelude::FromPrimitive, Decimal, RoundingStrategy};
-use std::{ error, io::{BufRead, BufReader, Read}, str::from_utf8};
+use std::error;
 use rustc_hash::FxHashMap;
 use memchr::memchr;
 extern crate num_cpus;
 use std::sync::mpsc;
 use std::thread;
-use rayon::prelude::*;
 
 
 
@@ -79,14 +79,22 @@ fn process_chunk(data: &[u8]) -> FxHashMap<&[u8], StationValues> {
 fn main() -> Result<(), Box<dyn error::Error>> {
     let args = Args::parse();
 
-    let mut file = std::fs::File::open(&args.file).expect("Failed to open file");
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).expect("Failed to read file");
+    use std::time::Instant;
+    let now = Instant::now();
+
+    let file = std::fs::File::open(&args.file).expect("Failed to open file");
+    
 
     let mut result: FxHashMap<&[u8], StationValues> = FxHashMap::default();
-    let mut buffer = &buffer[..];
 
-    // let (tx, rx) = mpsc::channel();
+    let mmap: Mmap;
+    let data: &[u8];
+
+    mmap = unsafe { Mmap::map(&file).unwrap() };
+    data = &*mmap;
+    let buffer = &data[..];
+
+    let (tx, rx) = mpsc::channel();
 
 
     // count logical cores this process could try to use
@@ -98,78 +106,85 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     while chunks_to_create > 0 && buffer.len() > 0 {
         let chunk_size = (buffer.len() / chunks_to_create) - 1;
         if buffer[chunk_size] == b'\n' {
-            // let chunk = &buffer[..chunk_size+1];
-            // spwan a thread to process chunk
-            // let tx = tx.clone();
-            // thread::scope(|scope| {
-            //     scope.spawn(move || {
-            //         let val = process_chunk(chunk);
-            //         tx.send(val).unwrap();
-            //     }); 
-            // });
             chunks.push((start, chunk_size+1));
             start = chunk_size + 1;
-
-            // buffer = &buffer[chunk_size+1..];
             chunks_to_create -= 1;
             chunks_created += 1;
         } else {
             let newline = memchr(b'\n', &buffer[chunk_size..]).unwrap();
-            // let chunk = &buffer[..chunk_size+newline+1];
-            // spwan a thread to process chunk
-            // let tx = tx.clone();
-            // thread::scope(|scope| {
-            //     scope.spawn(move || {
-            //         let val = process_chunk(chunk);
-            //         tx.send(val).unwrap();
-            //     }); 
-            // });
-
             chunks.push((start, chunk_size+newline+1));
             start = chunk_size+newline+1;
-
-            // buffer = &buffer[chunk_size+newline+1..];
             chunks_to_create -= 1;
             chunks_created += 1;
         }
     }
 
-    // let values: Vec<FxHashMap<&[u8], StationValues>> = chunks.into_par_iter().map(|(start, end)| process_chunk(&buffer[start..end])).collect();
-    // let values = chunks.into_par_iter().map(|(start, end)| process_chunk(&buffer[start..end]));
-    let values: Vec<FxHashMap<&[u8], StationValues>> = chunks.into_par_iter().map(|(start, end)| process_chunk(&buffer[start..end])).collect();
+    thread::scope(|scope| {
+        for (start, end) in chunks {
+            let tx = tx.clone();
+            scope.spawn(move || {
+                let val = process_chunk(&buffer[start..end]);
+                tx.send(val).unwrap();
+            }); 
+        }
+    });
 
-//     // println!("chunks created: {:?}", chunks_created);
-    for val in values{
-        for (station_name, station_values) in val.into_iter() {
-            result
-                .entry(station_name)
-                .and_modify(|e| {
-                    if station_values.min < e.min {
-                        e.min = station_values.min;
-                    }
-                    if station_values.max > e.max {
-                        e.max = station_values.max;
-                    }
-                    e.mean = e.mean + station_values.mean;
-                    e.count += station_values.count;
-                })
-                .or_insert(station_values);
+
+// RAYON CODE
+//     let values: Vec<FxHashMap<&[u8], StationValues>> = chunks.into_par_iter().map(|(start, end)| process_chunk(&buffer[start..end])).collect();
+
+//     for val in values{
+//         for (station_name, station_values) in val.into_iter() {
+//             result
+//                 .entry(station_name)
+//                 .and_modify(|e| {
+//                     if station_values.min < e.min {
+//                         e.min = station_values.min;
+//                     }
+//                     if station_values.max > e.max {
+//                         e.max = station_values.max;
+//                     }
+//                     e.mean = e.mean + station_values.mean;
+//                     e.count += station_values.count;
+//                 })
+//                 .or_insert(station_values);
         
+//     }
+// }
+
+
+   for _ in 0..chunks_created{
+    let val = rx.recv().unwrap();
+    for (station_name, station_values) in val.into_iter() {
+        result
+            .entry(station_name)
+            .and_modify(|e| {
+                if station_values.min < e.min {
+                    e.min = station_values.min;
+                }
+                if station_values.max > e.max {
+                    e.max = station_values.max;
+                }
+                e.mean = e.mean + station_values.mean;
+                e.count += station_values.count;
+            })
+            .or_insert(station_values);
     }
 }
 
 
-    for (_name, station_values) in result.iter_mut() {
-        // We want all values rounded to 1 decimal place  using the semantics of IEEE 754 rounding-direction "roundTowardPositive"
-        // Note: We use Decimal instaed of Floats, because it's easier to round up the fractional part of Decimals instead of floats
-        // Read later: https://users.rust-lang.org/t/why-doesnt-round-have-an-argument-for-digits/100688/24
-        let _max =  Decimal::from_f32(station_values.max).unwrap().round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
-        let _min =  Decimal::from_f32(station_values.min).unwrap().round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
-        let _mean =  Decimal::from_f32(station_values.mean/station_values.count).unwrap().round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
-    }
+for (_name, station_values) in result.iter_mut() {
+    // We want all values rounded to 1 decimal place  using the semantics of IEEE 754 rounding-direction "roundTowardPositive"
+    // Note: We use Decimal instaed of Floats, because it's easier to round up the fractional part of Decimals instead of floats
+    // Read later: https://users.rust-lang.org/t/why-doesnt-round-have-an-argument-for-digits/100688/24
+    let _max =  Decimal::from_f32(station_values.max).unwrap().round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
+    let _min =  Decimal::from_f32(station_values.min).unwrap().round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
+    let _mean =  Decimal::from_f32(station_values.mean/station_values.count).unwrap().round_dp_with_strategy(1, RoundingStrategy::MidpointAwayFromZero);
+}
 
 
 
-
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
     Ok(())
 }
